baa8d92bcbbc5310963ca05bef1f444473c7c33b
adongtt <240732222@qq.com>
2024-01-12 17:52:49 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,14 +10,14 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
+  siteTitle: "Cicada's Running Page",
   siteUrl: 'https://adongtt.github.io/cicada/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://adongtt.github.io/hansha/',
+      url: 'https://adongtt.github.io/cicada/',
     },
     {
       name: 'About',
