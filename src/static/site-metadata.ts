4e6e1d6e9c05dacf8abdb92679f6ff20928f4395
adongtt <240732222@qq.com>
2024-01-12 17:56:57 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,7 +12,7 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: "Cicada's Running Page",
   siteUrl: 'https://adongtt.github.io/cicada/',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  logo: 'https://www.custombaseballgloves.com/static/images/oldwang.jpg',
   description: 'Personal site and blog',
   navLinks: [
     {
